fb1b8e7ed1b6bff357074c66453c0fd37a23d6f0
Théo Marinho <thmarinho@outlook.fr>
2025-10-22 21:15:08 +0200

    Triggering CI

---
+++ backend/src/admin/vite.config.ts
@@ -0,0 +1,15 @@
+import { mergeConfig, type UserConfig } from 'vite';
+
+export default (config: UserConfig) => {
+  // Important: always return the modified config
+  return mergeConfig(config, {
+    server: {
+        allowedHost: true,
+    },
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
+    },
+  });
+};
