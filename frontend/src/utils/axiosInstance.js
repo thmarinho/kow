c94399a8d0f78e433f213290561cf001eef222a2
Théo Marinho <thmarinho@outlook.fr>
2025-10-26 21:33:01 +0100

    Triggering CI

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -3,11 +3,20 @@ import toast from "react-hot-toast";
 
 const axiosInstance = axios.create({
   baseURL: "https://kow-api.thmarinho.dev",
-  headers: {
-    Authorization: `Bearer ${Object.fromEntries(document.cookie.split('; ').map(e => e.split('='))).Authorization}`
-  }
 });
 
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = Object.fromEntries(document.cookie.split('; ').map(e => e.split('='))).Authorization
+
+    if (token)
+      config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 axiosInstance.interceptors.response.use(
   (success) => {
     return success;
