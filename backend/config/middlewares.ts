2a19c0b1c6c54b9adf753b0e49ac006c55ae4a4d
Théo Marinho <thmarinho@outlook.fr>
2025-11-13 10:29:40 +0100

    disable cors for 4321

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -2,6 +2,11 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
+  {
+    name: "strapi::cors",
+    origin: ['http://localhost:4321'],
+    headers: '*',
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
